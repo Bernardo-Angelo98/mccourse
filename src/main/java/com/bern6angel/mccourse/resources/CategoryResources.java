3a3276df8ba1814566f3b329ab7d9f21cb1be011
Bernardo-Angelo98 <bern6angel@gmail.com>
2020-11-12 17:47:37 +0000

    Rest Test

---
+++ src/main/java/com/bern6angel/mccourse/resources/CategoryResources.java
@@ -0,0 +1,16 @@
+package com.bern6angel.mccourse.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoryResources {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String list() {
+		return "Rest is working!";
+	}
+	
+}
